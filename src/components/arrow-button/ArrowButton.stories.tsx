9237cbf41a38eb546fdad5d8fe15d4c4ad131f57
Igor Grigorev <igory222222@yandex.ru>
2024-09-16 21:10:38 +0300

    braaaaaaaaaaaaaaanch

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,10 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton 
+					openPopup={false}
+					onClick={() => alert('')}
+				/>
 			</>
 		);
 	},
---
+++ src/components/article-params-form/hooks/useClosePopup.ts
@@ -0,0 +1,34 @@
+import { useLayoutEffect, RefObject } from 'react';
+
+type useClosePopupT = {
+  
+  openPopup: boolean
+  onClose: () => void
+  objectRef: RefObject<HTMLElement>
+}
+
+export function useClosePopup({openPopup, onClose, objectRef}: useClosePopupT) {
+  if (!openPopup) {false}
+
+  useLayoutEffect(() => {
+
+    function clickWhithoutBlock(evt: MouseEvent) {
+      const {target} = evt
+      const clickWhithoutBlockModel = target instanceof Node && objectRef.current && !objectRef.current.contains(target)
+
+      if(clickWhithoutBlockModel) {onClose()}
+    }
+
+    function escape (evt:KeyboardEvent) {
+      if(evt.key === "Escape") {onClose()}
+    }
+    
+    document.addEventListener('keydown', escape)
+    document.addEventListener('mousedown', clickWhithoutBlock)
+
+    return() => {
+      document.removeEventListener('keydown', escape)
+      document.removeEventListener('mousedown', clickWhithoutBlock)
+    }
+  })
+}
